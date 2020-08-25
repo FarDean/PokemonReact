8acf059e2870eaf029c8ab36d3a36e5194e98cd8
FarDean <sfh1212@gmail.com>
2020-08-25 19:54:24 +0430

    poke init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import PokemoneList from './components/PokemoneList';
+import axios from 'axios'
 
 function App() {
+  const [pokemon,setPokemon] = useState([])
+  axios.get('https://pokeapi.co/api/v2/pokemon').then(res=>{
+    setPokemon(res.data.results.map(p=> p.name))
+  })  
   return (
-
-    null
+    <PokemoneList pokemon={pokemon} />
   );
 }
 
